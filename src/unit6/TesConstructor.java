58115182a1360cbc44b5a0add8b0ad6dfc6a256b
chan21252 <chanjobs21252@gamil.com>
2019-05-09 21:57:49 +0800

    面向对象基础

--- src/unit6/TesConstructor.java
+++ src/unit6/TesConstructor.java
@@ -9,5 +9,11 @@ public class TesConstructor {
         System.out.println(emp.getClass().getName());
 
         Employee emp1 = new Employee("火云邪神", 10);
+        System.out.println(emp1.getName());
+        System.out.println(emp1.getWorkAge());
+        emp1.setSex('男');
+        System.out.println(emp1.getSex());
+
+        Employee emp2 = new Employee("火云邪神2", 12, '男');
     }
 }
--- src/unit6/entity/Employee.java
+++ src/unit6/entity/Employee.java
@@ -16,4 +16,38 @@ public class Employee {
         System.out.println("name 的值是 " + name);
         System.out.println("workAge 的值是 " + workAge);
     }
+
+    public Employee(String name, int workAge, char sex) {
+        this.name = name;
+        this.workAge = workAge;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        if (sex != '男' && sex != '女')
+            this.sex = '男';
+        else
+            this.sex = sex;
+    }
+
+    public int getWorkAge() {
+        return workAge;
+    }
+
+    public void setWorkAge(int workAge) {
+        this.workAge = workAge;
+    }
+
 }
